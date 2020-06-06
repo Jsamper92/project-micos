a0e27b3c14ac3e7fb3c7d417708c537c0ab98cdf
Tito Maquetas <jsamperarias@hotmail.com>
2020-06-07 01:51:54 +0200

    Insert animation scroll and rest of fragments page

---
+++ scripts/scroll.js
@@ -0,0 +1,12 @@
+const header = document.querySelector('.header__nav');
+
+document.addEventListener('scroll', _ => {
+    console.log(event);
+    if(scrollY >= 155) {
+        header.classList.add('scale-header-on');
+    } else {
+        header.classList.remove('scale-header-on');
+    }
+})
+
+

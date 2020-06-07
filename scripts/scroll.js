4d10cc7d14ab009aede1a3a2f1de15af0c4baec2
Tito Maquetas <jsamperarias@hotmail.com>
2020-06-07 19:05:08 +0200

    Finished v1

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,12 +1,10 @@
-const header = document.querySelector('.header__nav');
+const header = document.querySelector(".header__nav");
 
-document.addEventListener('scroll', _ => {
-    console.log(event);
-    if(scrollY >= 155) {
-        header.classList.add('scale-header-on');
-    } else {
-        header.classList.remove('scale-header-on');
-    }
-})
-
-
+document.addEventListener("scroll", (_) => {
+  console.log(event);
+  if (scrollY >= 155) {
+    header.classList.add("scale-header-on");
+  } else {
+    header.classList.remove("scale-header-on");
+  }
+});

8587114c1529f2e69d603123b86651d5205081a8
Javier Samper Arias <javier.samper.arias@nttdata.com>
2024-05-25 14:26:12 +0200

    feat: add plugin robotstxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react({
     experimentalReactChildren: true
-  })],
+  }), robotsTxt()],
   output: "server",
   adapter: vercel()
 });
